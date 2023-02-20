561be14b4fe5f592dc933500eef170f3972f71a9
Leo Lee <lee3266@purdue.edu>
2023-02-19 21:19:52 -0500

    this is a thing

--- src/pages/ContactUs/ContactUs.js
+++ src/pages/ContactUs/ContactUs.js
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import "./ContactUs.css";
+import { useAuth } from "../../context/AuthContext";
+import { db } from "../../firebase/config";
 
 const ContactUs = () => {
+  const auth = useAuth();
   const [input, setInput]=useState('');
   const [email, setEmail]=useState('');
 
   const handleSubmit = () => {
+
+    db.collection("ContactUs").add({
+      Email: email,
+      Message: input,
+    });
+    setEmail('');
+    setInput('');
+    
   };
 
 
   return (
-    <form
-    className="box"
-    >
+    <form className="box">
         <div>
             Contact Us
         </div>
@@ -21,7 +30,7 @@ const ContactUs = () => {
       </div>
 
       <div>
-        <textarea placeholder="Message here" value={input} onChange={e=>setInput(e.target.value)}required />
+        <input placeholder="Message here" value={input} onChange={e=>setInput(e.target.value)}required />
       </div>
       <div>
         <button onClick={handleSubmit}> Send a message </button>
